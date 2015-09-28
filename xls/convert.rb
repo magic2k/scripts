7888a2f197de656b3c4cd86824dc0294a73d53d8
magic2k <magic2ktech@gmail.com>
2015-09-28 22:16:53 +0300

    xls

--- xls/convert.rb
+++ xls/convert.rb
@@ -1,5 +1,9 @@
 require 'rubygems'
 require 'simple_xlsx_reader'
 
-document = SimpleXlsxReader.open('./gamma1.xlsx')
+#document = SimpleXlsxReader.open('./gamma1.xlsx')
+document = SimpleXlsxReader.open('./gamma_sample.xlsx')
+
+puts document.sheets.first.rows.inspect
+
 
