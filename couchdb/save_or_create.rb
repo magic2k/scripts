0d29c8b69e68be87e02b4c718e1d19de44f5f042
magic2k <magic2ktech@gmail.com>
2016-06-16 10:44:19 +0300

    added couchdb create_or_save example

---
+++ couchdb/save_or_create.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'couchrest'
+#require 'date'
+
+server = CouchRest.new
+#create db if it doesn't already exist
+db = server.database!('test_db')
+
+begin
+  doc = db.get('doc')
+rescue RestClient::ResourceNotFound => nfe
+  #Save document, with id
+  db.save_doc('_id' => 'doc', 'name' => 'test', 'date' => DateTime.now)
+  doc = db.get('doc')
+end
+
+puts doc.inspect
+#db.delete_doc(doc)
+
+
