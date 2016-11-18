5f67d40399f35033b27b68b762069a1c212e5d38
Vladimir <magic2ktech@gmail.com>
2016-11-18 09:51:06 +0300

    Update rubyjson.rb
    added json example

--- misc/rubyjson.rb
+++ misc/rubyjson.rb
@@ -1,7 +1,14 @@
 require 'rubygems'
 require 'json'
 
-json =ARGV[0]
-parsed_json = JSON.parse(json)
-return parsed_json["public_url"]
+def parseSimpleJson json
+  #json =ARGV[0]
+  parsed_json = JSON.parse(json)
+  return parsed_json["public_url"]
+end
 
+def getNestedHashFromJsonElement json
+  parsed_json = JSON.parse(json)
+  results_hash = parsed_json['someElement'][0]
+  return results_hash
+end
